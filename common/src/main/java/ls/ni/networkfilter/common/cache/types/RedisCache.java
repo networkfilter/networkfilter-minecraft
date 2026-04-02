8ceb42834e6f27dd247b2f418f1451b33e87abc7
Nycz-lab <Skybreak00@protonmail.com>
2026-04-02 14:34:02 +0200

    feat: implement sentineled connection for redis cache

--- common/src/main/java/ls/ni/networkfilter/common/cache/types/RedisCache.java
+++ common/src/main/java/ls/ni/networkfilter/common/cache/types/RedisCache.java
@@ -5,26 +5,60 @@ import ls.ni.networkfilter.common.cache.Cache;
 import ls.ni.networkfilter.common.filter.FilterResult;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.jetbrains.annotations.NotNull;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.*;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.Set;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class RedisCache implements Cache {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-    private final JedisPool jedisPool;
+    private final UnifiedJedis unifiedJedis;
     private final Duration expireAfterWrite;
 
+    private static final int SENTINEL_DEFAULT_PORT = 26379;
+
     public RedisCache(@NotNull String uri, @NotNull Duration expireAfterWrite) {
-        this.jedisPool = new JedisPool(uri);
+        this.unifiedJedis = new JedisPooled(uri);
 
         this.expireAfterWrite = expireAfterWrite;
 
         // Close the JedisPool when the JVM is shutting down
-        Runtime.getRuntime().addShutdownHook(new Thread(this.jedisPool::close));
+        Runtime.getRuntime().addShutdownHook(new Thread(this.unifiedJedis::close));
+    }
+
+    public RedisCache(String masterName, List<String> sentinelAddresses, String username, String password,
+                     boolean ssl, String sentinelUsername, String sentinelPassword, Duration expireAfterWrite){
+
+        this.expireAfterWrite = expireAfterWrite;
+
+        Set<HostAndPort> sentinels = sentinelAddresses.stream()
+                .map(addr -> parseAddress(addr))
+                .collect(Collectors.toSet());
+
+        this.unifiedJedis = new JedisSentineled(masterName, jedisConfig(username, password, ssl),
+                sentinels, jedisConfig(sentinelUsername, sentinelPassword, ssl));
+    }
+
+    private static HostAndPort parseAddress(String address) {
+        return parseAddress(address, Protocol.DEFAULT_PORT);
+    }
+
+    private static HostAndPort parseAddress(String address, int defaultPort) {
+        return new HostAndPort(address, defaultPort);
+    }
+
+    private static JedisClientConfig jedisConfig(String username, String password, boolean ssl) {
+        return DefaultJedisClientConfig.builder()
+                .user(username)
+                .password(password)
+                .ssl(ssl)
+                .timeoutMillis(Protocol.DEFAULT_TIMEOUT)
+                .build();
     }
 
     @Override
@@ -36,12 +70,12 @@ public class RedisCache implements Cache {
     public @Nullable FilterResult getIfPresent(@NotNull String key) {
         String cacheKey = this.cacheKey(key);
 
-        try (Jedis resource = this.jedisPool.getResource()) {
-            if (!resource.exists(cacheKey)) {
+        try {
+            if (!this.unifiedJedis.exists(cacheKey)) {
                 return null;
             }
 
-            return this.objectMapper.readValue(resource.get(cacheKey), FilterResult.class);
+            return this.objectMapper.readValue(this.unifiedJedis.get(cacheKey), FilterResult.class);
         } catch (Throwable cause) {
             throw new RuntimeException("Failed to get value from Redis", cause);
         }
@@ -64,10 +98,11 @@ public class RedisCache implements Cache {
     public void put(@NotNull String key, @NotNull FilterResult value) {
         String cacheKey = this.cacheKey(key);
 
-        try (Jedis resource = this.jedisPool.getResource()) {
+        try {
             String mappedValue = this.objectMapper.writeValueAsString(value);
 
-            resource.setex(cacheKey, (int) this.expireAfterWrite.getSeconds(), mappedValue);
+            this.unifiedJedis.setex(cacheKey, (int) this.expireAfterWrite.getSeconds(), mappedValue);
+
         } catch (Throwable cause) {
             throw new RuntimeException("Failed to put value to Redis", cause);
         }
